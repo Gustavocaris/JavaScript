a7ea242e6af83384723dacb03abb8d54b32f6b2a
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-02 20:41:00 -0300

    Estruturando a funcção

--- Exercicios/03-Tabuada/funcao.js
+++ Exercicios/03-Tabuada/funcao.js
@@ -2,6 +2,8 @@ function Tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     if (num.value.length == 0) {
-        window.alert()
+        window.alert('Por favor digite um numero!')
+    } else {
+        let n = Number(num.value)
     }
 }
