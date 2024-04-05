96904e24617debbc7f4bab07f8ef9bf2202c8093
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-04 22:44:34 -0300

    Adicionando os arquivos que eu tinha de HTML e CSS

---
+++ ONE/01 - Logica com JS/01 - Lógica com JS/app.js
@@ -0,0 +1,18 @@
+
+/*let senhaDoSistema = 5;
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha == senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+} else {
+    alert("Senha incorreta, tente novamente");
+} */
+
+let nummaximo = prompt("Digite um número para a contagem regressiva");
+let contador = 0;
+
+while (contador <= nummaximo) {
+    console.log(contador);
+    contador++
+}
---
+++ ONE/01 - Logica com JS/02 - Lógica com JS parte2/app.js
@@ -0,0 +1,19 @@
+/*let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogo do numero secreto';
+
+let paragrafo = document.querySelector('p')
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+*/
+
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do numero secreto');
+exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
