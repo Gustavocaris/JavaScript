e44e86c9e8579f6cae64978414fef134cdd8d4e1
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-14 16:57:49 -0300

    Estabelecendo uma estrutura para acessar os elemento de forma dinamica

--- 02---JavaScript---/ALURA/aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
+++ 02---JavaScript---/ALURA/aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,7 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom (idElementoAudio) {
+    /*parametro-> nossa função vai depender desse parametro(idElementoAudio)  */
 
+    document.querySelector(idElementoAudio).play()
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom // não preciso colocar o parenteses, pois ele tem que guardar essa informação
@@ -9,4 +10,24 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom // não preciso coloca
 /*referencias devem ser criadas com base nos valores que serão guardadas*/
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
+let contador = 0
 
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+    
+    console.log(instrumento)
+
+    console.log(idAudio)
+
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    contador = contador +1
+
+    console.log(contador)
+}
