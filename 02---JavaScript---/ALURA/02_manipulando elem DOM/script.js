51ddc857a1d566c30079c8c83994951c9fb3dee2
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-26 19:23:36 -0300

    refatorando elementos da funcão

--- 02---JavaScript---/ALURA/02_manipulando elem DOM/script.js
+++ 02---JavaScript---/ALURA/02_manipulando elem DOM/script.js
@@ -1,24 +1,52 @@
 // Buscando os elementos do nosso HTML
 
 const html = document.querySelector('html')
-const focoBT = document.querySelector('.app__card-button--foco')
-const curtoBT = document.querySelector('.app__card-button--curto')
-const longoBT = document.querySelector('.app__card-button--longo')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+
 
 // Criando nossos eventos
 // EVENTO DE CLICK -> vou chamar o addeventlistener e passar uma função para quando 
 // tiver um click nesse botão
-focoBT.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco') // pegar a variavel HTML e alterar seu atributo
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco')
 })
 
-curtoBT.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
 })
 
-longoBT.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+longoBt.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
 })
 
 
+// pegar meu html e alterar o contexto, o addeventlistener estava muito repetitivo
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            // innerHTML -> especifico para textos, classes, lista em html, peguei do jeito que esta no HTML
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+            break;
+    }
+}
 
