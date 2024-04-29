11ebdc0b5df5a08074bea335d45d90798f857c98
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-29 18:41:50 -0300

    iniciando o metodo Sort

---
+++ 02---JavaScript---/ALURA/03_metodosArray/app/sort.js
@@ -0,0 +1,4 @@
+/* O método sort() é um método de array em JavaScript que é usado para 
+ordenar os elementos de um array. Ele pode ser utilizado para ordenar 
+strings, números e até mesmo objetos de acordo com um critério de ordenação específico*/
+
