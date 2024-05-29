e91a64fbd4ec17cd2e396b32dc54bb884d9a16c3
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-05-28 21:45:50 -0300

    Function para exportar o banner do projeto

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+//  A gente tem uma função e estamos exportando e retornando um componente 
+function Banner() {
+    // JSX
+    return (
+        <img src="/imagens/banner.png" alt="logo" />
+    )
+}
+
+export default Banner
