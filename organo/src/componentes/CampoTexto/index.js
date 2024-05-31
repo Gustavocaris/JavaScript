1b8089bc8a7265996e01199bcf73309262d6ed80
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-05-31 17:31:20 -0300

    manipulando o input do Usuario

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -2,12 +2,22 @@ import './CampoTexto.css'
 
 // esse 'props' poderia ser qualquer nome, apenas pra chamar
 const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...` 
+
+    let valor = 'Guilherme Silveira'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}></input>
 
         </div>
     )
