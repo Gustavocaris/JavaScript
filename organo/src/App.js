2207e4b256564142cca426ad682f62100134c83c
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-05-30 22:29:27 -0300

    Criando o formulario

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,17 +1,13 @@
 
-import Banner from './componentes/Banner/Banner/Banner';
-import CampoTexto from './componentes/Banner/campoTexto';
+import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario';
+
 
 function App() {
   return (
     <div className="App">
-        <div className="App">
-          <Banner />
-          <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
-          <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
-          <CampoTexto label="Imagem" placeholder="Digite seu Imagem" />
-
-        </div>
+      <Banner />
+      <Formulario />
     </div>
   );
 }
