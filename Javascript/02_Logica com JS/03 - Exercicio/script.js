095b9e2ce0f68bb6b95ce379dddfe70b37892aa0
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-08-24 17:49:29 -0300

    Estruturando a function com as estruturas

--- Javascript/02_Logica com JS/03 - Exercicio/script.js
+++ Javascript/02_Logica com JS/03 - Exercicio/script.js
@@ -4,9 +4,34 @@ function calculadora() {
     let num2 = parseFloat(prompt("Digite o segundo número:  "));
 
     // perguntar qual operação ele deseja fazer
-    let operacao = parseInt(prompt("Escolha qual operação deseja fazer ?"))
+    let operacao = prompt("Escolha qual operação deseja fazer: - + / * ?")
 
-
-
-
+    //Realizar as operacoes e exibir na telovix
+    let resultado;
+    switch (operacao) {
+        case "-":
+            resultado = num1 - num2;
+            break;
+        case "+":
+            resultado = num1 + num2;
+            break;
+        case "*":
+            resultado = num1 * num2;
+            break;
+        case "/":
+            if (num2 === 0) {
+                alert("Divisão por zero não é permitida.");
+                return;
+            } else {
+                resultado = num1 / num2;
+            }
+            break;
+        default:
+            alert("Operação inválida.");
+            return;
+    }
+    // Exibe o resultado
+    alert("Resultado: " + resultado);
 }
+
+calculadora();
