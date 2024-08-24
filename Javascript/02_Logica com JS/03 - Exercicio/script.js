7b50b4aa05e4fa636b4a98a99fe744af72a4a78f
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-08-24 17:22:36 -0300

    problema simples sobre uma calculadora

---
+++ Javascript/02_Logica com JS/03 - Exercicio/script.js
@@ -0,0 +1,12 @@
+function calculadora() {
+    // Solicitar os numeros ao usuario
+    let num1 = parseFloat(prompt("Digite o primeiro número: "));
+    let num2 = parseFloat(prompt("Digite o segundo número:  "));
+
+    // perguntar qual operação ele deseja fazer
+    let operacao = parseInt(prompt("Escolha qual operação deseja fazer ?"))
+
+
+
+
+}
