2c897eff57f8ea815fea237af8ce625fdc75bee6
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-12-03 22:41:00 -0300

    organizando a pasta e anotando as aulas

---
+++ Javascript/11_avancando/03-reduce/index.js
@@ -0,0 +1,56 @@
+let ordens = [
+  // array com alguns objetos
+  {
+    cliente: "Roberto",
+    tipo: "compra",
+    quantidade: 56,
+    ativo: "NFLX34",
+  },
+  {
+    cliente: "Ricardo",
+    tipo: "compra",
+    quantidade: 76,
+    ativo: "AAPL34",
+  },
+  {
+    cliente: "Raphael",
+    tipo: "venda",
+    quantidade: 21,
+    ativo: "GOGL34",
+  },
+];
+
+/* 
+
+// ----------> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+>>> SEM REDUCE:
+
+let quantidadeDeOrdens = 0;
+
+for (let i = 0; i < ordens.length; i++) {
+  quantidadeDeOrdens += ordens[i].quantidade;
+}
+
+console.log(quantidadeDeOrdens); 
+
+// Output: 153
+
+
+>>> COM REDUCE: 
+
+*/
+
+let totalDeOrdens = ordens.reduce(function (ordemAnterior, ordemAtual) {
+  return ordemAnterior + ordemAtual.quantidade;
+}, 0);
+
+console.log(totalDeOrdens);
+
+/*
+
+>>> REDUCE + ARROW FUNCTION:
+
+let totalDeOrdens = ordens.reduce((ordemAnterior, ordemAtual) => OrdemAnterior + ordemAtual.quantidade, 0);
+
+*/
