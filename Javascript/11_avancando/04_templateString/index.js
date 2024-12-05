256784e0cab4d3bdec0ad5edf400a46098aa52ae
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-12-04 21:53:24 -0300

    Interpolacao de strings

---
+++ Javascript/11_avancando/04_templateString/index.js
@@ -0,0 +1,22 @@
+/* 
+
+>>> Concatenação (método antigo, até o ES6):
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+  return (
+    "Primeiro nome: " +
+    primeiroNome +
+    ". Último nome: " +
+    ultimoNome +
+    ". Idade: " +
+    idade
+  );
+};
+
+console.log(imprimirInformacoesPessoa("Pablo", "Viana", 26));
+
+Output: "Primeiro nome: Pablo. Último nome: Viana. Idade: 26"
+
+>>> Interpolação (método atualizado):
+
+*/
