47f6da40e1632092d3bf9df33dfd700cf7399c2d
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-12-06 00:09:35 -0300

    final js

--- Javascript/11_avancando/05-desestruturacao-objetos-arrays/index.js
+++ Javascript/11_avancando/05-desestruturacao-objetos-arrays/index.js
@@ -23,3 +23,20 @@ let { nome: nome, idade: idade } = pessoa;
 console.log(nome);
 console.log(idade);
 
+/* Sendo o valor antes dos dois pontos a propriedade que queremos pegar e o valor após os dois pontos o nome que queremos dar para a variável. Após o símbolo de igual deve estar o objeto de onde queremos retirar as propriedades. Além disso, caso o nome da propriedade seja igual ao nome da variável é possível escrever apenas o nome da propriedade, dessa forma:
+
+let { nome, idade } = pessoa; 
+
+Caso seja diferente, é necessário utilizar os dois pontos:
+
+let { nome, idade: idadeDaPessoa } = pessoa; 
+
+>>> Método para criar variáveis cujos valores são itens de um array:
+
+*/
+
+const numeros = [1, 2, 3];
+
+const [um, dois, tres] = numeros;
+
+console.log(um, tres); // Output: 1, 3
