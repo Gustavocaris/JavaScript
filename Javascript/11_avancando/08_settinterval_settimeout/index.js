ed2b1376dbb4639d7767e90228a082e1007469ab
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-12-08 14:51:39 -0300

    settimeout and setinterval

---
+++ Javascript/11_avancando/08_settinterval_settimeout/index.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    alert('Teste, olá, testando')
+}, 2000);
+// sempre em milisegundos, no caso seria 02 segundos
+
+
+setInterval(() => {
+    console.log('Executando a cada 2s')
+}, 2000);
+// executar esse console a cada 2s
