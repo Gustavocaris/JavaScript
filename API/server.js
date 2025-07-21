73051d1848fe5bb57357d4448e199ce7741e98a1
Gustavo <gustavocaris14052002@gmail.com>
2025-07-21 16:37:59 -0300

    Iniciando a contrução da API fazendo a requisiçsao dos usuarios

--- API/server.js
+++ API/server.js
@@ -2,9 +2,17 @@ import express, { response } from 'express';
 
 const app = express();
 
+const user = []
+
+app.post('/usuarios',(request,response) => {
+    console.log(request)
+})
+
 // na nossa rota: Tipo e o endereço
 app.get('/usuarios',(request,response) => {
     app.send('Teste de comunicacao');
 })
 
 app.listen(3000);
+
+// Criando nossa API de usuarios - Mais info no Doc.txt
